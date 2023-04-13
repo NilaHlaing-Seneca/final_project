ddf82ef9b59172139c085a49e1bc47fad7ad4690
NilaHlaing-Seneca <122833089+NilaHlaing-Seneca@users.noreply.github.com>
2023-04-13 03:03:50 -0400

    Add us-east-1c Public and Private Subnets

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -22,4 +22,30 @@ resource "aws_subnet" "zone1_private_sn1" {
   }
 }
 
+# Create  Subnet Public_SN1 in us-east-1c
+resource "aws_subnet" "zone2_public_sn1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.2.2.0/24"
+  map_public_ip_on_launch = "true" #it makes this a public subnet
+  availability_zone       = "us-east-1c"
+  tags = {
+    Name = "Zone-2 Public_SN1"
+  }
+}
+
+# Create  Subnet Private_SN1 in us-east-1c
+resource "aws_subnet" "zone2_private_sn1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.2.4.0/24"
+  map_public_ip_on_launch = "true" #it makes this a public subnet
+  availability_zone       = "us-east-1c"
+  tags = {
+    Name = "Zone-2 Private_SN1"
+  }
+}
+
+
+
+
+
 # -------------------- End -------------------------
