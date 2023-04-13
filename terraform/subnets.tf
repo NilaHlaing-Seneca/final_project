7d8307936c756750799058a0df872f5e980b1b2b
NilaHlaing-Seneca <122833089+NilaHlaing-Seneca@users.noreply.github.com>
2023-04-13 03:45:28 -0400

    Added all the Subnets

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -44,8 +44,26 @@ resource "aws_subnet" "zone2_private_sn1" {
   }
 }
 
+# Create  Subnet Public_SN1 in us-east-1d
+resource "aws_subnet" "zone3_public_sn1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.2.5.0/24"
+  map_public_ip_on_launch = "true" #it makes this a public subnet
+  availability_zone       = "us-east-1d"
+  tags = {
+    Name = "Zone-3 Public_SN1"
+  }
+}
 
-
-
+# Create  Subnet Private_SN1 in us-east-1d
+resource "aws_subnet" "zone3_private_sn1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.2.7.0/24"
+  map_public_ip_on_launch = "true" #it makes this a public subnet
+  availability_zone       = "us-east-1d"
+  tags = {
+    Name = "Zone-3 Private_SN1"
+  }
+}
 
 # -------------------- End -------------------------
