809cbbd79e43b283fe478c9f19174cc47a31e3ed
NilaHlaing-Seneca <122833089+NilaHlaing-Seneca@users.noreply.github.com>
2023-04-15 09:52:50 -0400

    staging is now in new branch. no longer needed in here

--- staging/config.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "final-1"         // Bucket where to SAVE Terraform State
-    key    = "webservers/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
-    region = "us-east-1"                        // Region where bucket is created
-  }
-}
--- staging/keypair.tf
+++
@@ -1,5 +0,0 @@
-#Resource to create a SSH private key
-resource "aws_key_pair" "group7_keypair" {
-  key_name   = "group7"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDSCc9RZOhOjEeWqbsdCJUGtbHThZ1hMNUkQZRB1xqG2FZGWjePWRtONGYNQ5YCDYdrdyl23atdP5waLx7Tr9ZevHLFpUikGLA1NZOGlQkCTDXJmZ9SDjrgMSMMWjAsBXaAzmE/qrwA9xthzzEfPcKTrmxbeZ+rZ2dR2bMTz+M9bYRV4d08cBlLxxNO69lPRcN0wYnVv7HY5/JtQHmVmRQnK6/rsle89GQuPX2zjTTRX8ksl9FgQlUyq1c8NXoJtc7R0wT7I2j5UD+pF3iDaemOW66OvA+/mRNWFjpdM4aEeWyJq7zoe1+gmVP/heEYpKNj0g5lSSVRjnqUBvaE/6fz ec2-user@ip-172-31-21-24.ec2.internal"
-  }
