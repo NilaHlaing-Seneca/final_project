fddac95353f1d781552f5f2ad4bb32f869161c37
Arunadevi-Krishnan <126430166+Arunadevi-Krishnan@users.noreply.github.com>
2023-04-14 20:14:11 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,23 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+# Elastic IP
+resource "aws_eip" "static_eip" {
+  instance = aws_instance.zone2_vm1_instance.id
+  tags = merge(var.default_tags,
+    {
+      "Name" = "var.prefix-eip"
+    }
+  )
+}
+
+#Webserver homepage
+data "template_file" "install_httpd" {
+  template = file("${path.module}/install_httpd.sh.tpl")
+}
+
+#Elastic IP update
+resource "aws_eip" "nat_gateway" {
+  vpc      = true
+}
